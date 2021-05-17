298a70b4746f039e676ca5e3c3e3cb291b66bf07
do_xuan_trung_iMac <trung13988@gmail.com>
2021-05-17 17:00:12 +0700

    ROLLUP CONFIG - edited

--- rollup.config.js
+++ rollup.config.js
@@ -6,40 +6,36 @@ import resolve from "rollup-plugin-node-resolve";
 import pkg from "./package.json";
 
 export default {
-  input: "src/index.tsx",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      exclude: "**/__tests__/**",
-      clean: true,
-    }),
-    commonjs({
-      include: ["node_modules/**"],
-      namedExports: {
-        "node_modules/react/react.js": [
-          "Children",
-          "Component",
-          "PropTypes",
-          "createElement",
-        ],
-        "node_modules/react-dom/index.js": ["render"],
-      },
-    }),
-  ],
+    input: "src/index.tsx",
+    output: [
+        {
+            file: pkg.main,
+            format: "cjs",
+            exports: "named",
+            sourcemap: true,
+        },
+        {
+            file: pkg.module,
+            format: "es",
+            exports: "named",
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        external(),
+        resolve(),
+        typescript({
+            rollupCommonJSResolveHack: true,
+            exclude: "**/__tests__/**",
+            clean: true,
+        }),
+        commonjs({
+            include: ["node_modules/**"],
+            namedExports: {
+                "node_modules/react/react.js": ["Children", "Component", "PropTypes", "createElement"],
+                "node_modules/react-dom/index.js": ["render"],
+                "node_modules/react-is/index.js": ["isFragment", "ForwardRef", "isMemo"],
+            },
+        }),
+    ],
 };
