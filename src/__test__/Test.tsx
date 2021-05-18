ec2c06d1160b44d66daa11a403dd0bf1530d31a8
do_xuan_trung_iMac <trung13988@gmail.com>
2021-05-18 10:49:28 +0700

    TEST - add button

--- src/__test__/Test.tsx
+++ src/__test__/Test.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import InputText from "../components/input/InputText";
 import Icon, { IconProps } from "../components/icon/Icon";
+import Button from "../components/button/Button";
 
 interface Props {
     content?: any;
@@ -8,9 +9,14 @@ interface Props {
 
 export default function Test({ content }: Props): ReactElement {
     return (
-        <div>
+        <div className="p-5">
             <InputText />
             <Icon name="menu" />
+            <div className="d-flex align-items-center">
+                <Button content="Button" />
+                <Button content="Icon Button" className="mx-3" iconName="home" />
+                <Button content="Disabled Button" className="mx-3" disabled />
+            </div>
         </div>
     );
 }
