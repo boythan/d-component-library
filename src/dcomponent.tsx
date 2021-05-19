ce869bec786aba2be7157a7ec902804c01b1545f
vinhdx <vinhdx.k57@gmail.com>
2021-05-19 08:45:44 +0700

    SELECT - add tree select

--- src/dcomponent.tsx
+++ src/dcomponent.tsx
@@ -1,19 +1,40 @@
 import "./scss/index.scss";
 
+/* ----------------------------------------
+// Input
+// ---------------------------------------- */
 export type { InputTextProps } from "./components/input/InputText";
 export { default as InputText } from "./components/input/InputText";
 
+/* ----------------------------------------
+// Icon
+// ---------------------------------------- */
 export type { IconProps } from "./components/icon/Icon";
 export { default as Icon } from "./components/icon/Icon";
 
+/* ----------------------------------------
+// Select
+// ---------------------------------------- */
 export type { SelectProps } from "./components/select/Select";
 export { default as Select } from "./components/select/Select";
 
+export type { TreeSelectProps } from "./components/select/TreeSelect";
+export { default as TreeSelect } from "./components/select/TreeSelect";
+
+/* ----------------------------------------
+// Button
+// ---------------------------------------- */
 export type { ButtonProps } from "./components/button/Button";
 export { default as Button } from "./components/button/Button";
 
+/* ----------------------------------------
+// Checkbox
+// ---------------------------------------- */
 export type { CheckboxProps } from "./components/checkbox/Checkbox";
 export { default as Checkbox } from "./components/checkbox/Checkbox";
 
+/* ----------------------------------------
+// Header
+// ---------------------------------------- */
 export type { HeaderTableProps } from "./components/header/HeaderTable";
 export { default as HeaderTable } from "./components/header/HeaderTable";
