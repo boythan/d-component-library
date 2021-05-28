eceb2e0b1e41b3d0e7d7ee4bc0650e5d0661a702
do_xuan_trung_iMac <trung13988@gmail.com>
2021-05-28 14:23:55 +0700

    DOT - add className props

--- src/components/dot/Dot.tsx
+++ src/components/dot/Dot.tsx
@@ -5,10 +5,11 @@ import { ButtonProps } from "../button/Button";
 export interface DotProps {
     color?: ButtonProps["color"];
     size?: ButtonProps["size"];
+    className?: string;
 }
 
-const Dot: React.FC<DotProps> = ({ size = "small", color = "green" }) => {
-    const dotClass = ClassNames("d-dot_container", `d-dot__${size}-${color}`);
+const Dot: React.FC<DotProps> = ({ size = "small", color = "green", className }) => {
+    const dotClass = ClassNames("d-dot_container", `d-dot__${size}-${color}`, className);
     return <div className={dotClass} />;
 };
 
