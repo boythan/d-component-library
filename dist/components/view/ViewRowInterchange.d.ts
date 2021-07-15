9a6f246733df582657cc669e837c97ea8b49e75c
vinhdx <vinhdx.k57@gmail.com>
2021-07-15 09:13:09 +0700

    UPGRADE - 0.1.93

---
+++ dist/components/view/ViewRowInterchange.d.ts
@@ -0,0 +1,31 @@
+import React, { CSSProperties } from "react";
+export interface IRowsKey<T> {
+    id: keyof T;
+    label: string | number;
+    renderLabel?: (props: {
+        id: IRowsKey<T>["id"];
+        data: any;
+        row?: IRowsKey<T>;
+    }) => any;
+    renderContent?: (props: {
+        id: IRowsKey<T>["id"];
+        data: any;
+        row?: IRowsKey<T>;
+    }) => any;
+}
+export interface RowInterchangeViewProps {
+    className?: string;
+    classNameRow?: string;
+    classNameLabel?: string;
+    classNameContent?: string;
+    dataSource: any;
+    keyList: Array<IRowsKey<any>>;
+    variant?: "background" | "border" | "dashed" | "none";
+    boldLabel?: boolean;
+    Messages?: any;
+    style?: CSSProperties;
+    styleContent?: CSSProperties;
+    styleLabel?: CSSProperties;
+}
+declare const ViewRowInterchange: React.FC<RowInterchangeViewProps>;
+export default ViewRowInterchange;
