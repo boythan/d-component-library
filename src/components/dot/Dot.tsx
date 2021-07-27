7b0dcdc5fd1dc8f64826dd8180eb3ce7edaa201e
do_xuan_trung_iMac <trung13988@gmail.com>
2021-07-27 16:05:20 +0700

    DOT - edit props

--- src/components/dot/Dot.tsx
+++ src/components/dot/Dot.tsx
@@ -8,14 +8,16 @@ export interface DotProps {
     size?: IconProps["size"];
     className?: string;
     style?: CSSProperties;
-    children?: string | number;
+    children?: any;
+    index?: any;
 }
 
-const Dot: React.FC<DotProps> = ({ size = "small", color = "green", className, style, children }) => {
+const Dot: React.FC<DotProps> = ({ size = "small", color = "green", className, style, children, index }) => {
     const dotClass = ClassNames("d-dot_container", `d-dot__${size}-${color}`, className);
     return (
         <div className={dotClass} style={style}>
             {children}
+            {index}
         </div>
     );
 };
