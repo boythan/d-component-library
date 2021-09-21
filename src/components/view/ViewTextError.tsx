40688a5a9dd4ba50ec790d1c7cf6bca7cd0651e9
vinhdx <vinhdx.k57@gmail.com>
2021-09-21 10:16:29 +0700

    v 0.2.32

--- src/components/view/ViewTextError.tsx
+++ src/components/view/ViewTextError.tsx
@@ -5,12 +5,14 @@ export interface ViewTextErrorProps {
     error: any;
 }
 
-const ViewTextError = ({ error }: ViewTextErrorProps) =>
-    error && (
+const ViewTextError = ({ error }: ViewTextErrorProps) => {
+    if (!error) return <div />;
+    return (
         <div className="flex-center-y mt-1" hidden={!error}>
             <Icon name="error_outline" className="text-error" size="small" />
             <text className="text-x-small text-error ml-1">{error}</text>
         </div>
     );
+};
 
 export default ViewTextError;
