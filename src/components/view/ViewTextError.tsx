28145872d63044c650c7fc4fc5458e400d46e83f
vinhdx <vinhdx.k57@gmail.com>
2021-09-21 09:57:39 +0700

    ERROR TEXT VIEW - hide when have no error

--- src/components/view/ViewTextError.tsx
+++ src/components/view/ViewTextError.tsx
@@ -5,11 +5,12 @@ export interface ViewTextErrorProps {
     error: any;
 }
 
-const ViewTextError = ({ error }: ViewTextErrorProps) => (
-    <div className="flex-center-y mt-1" hidden={!error}>
-        <Icon name="error_outline" className="text-error" size="small" />
-        <text className="text-x-small text-error ml-1">{error}</text>
-    </div>
-);
+const ViewTextError = ({ error }: ViewTextErrorProps) =>
+    error && (
+        <div className="flex-center-y mt-1" hidden={!error}>
+            <Icon name="error_outline" className="text-error" size="small" />
+            <text className="text-x-small text-error ml-1">{error}</text>
+        </div>
+    );
 
 export default ViewTextError;
